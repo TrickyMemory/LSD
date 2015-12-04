cc168bec45d37850220a37459d2837f257e7f943
Marie Mion <Marie Mion>
2015-12-04 05:20:45 +0100

    les rapports à rendre

---
+++ site/moncompte.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="favicon.ico">
+  <title>Les Sockets Diurnes</title>
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <!-- Custom styles for this template -->
+  <link href="css/monStyle.css" rel="stylesheet">
+</head>
+<body>
+	<?php
+		include "top.php";
+	?>
+    <div id="wrapper">
+	<?php
+		include "menu.php";
+	?>
+    </div>
+    <div id="wrapper">
+      <div class="container-fluid">
+        <div class="row">
+          <h1>Mon Compte</h1>
+        </div>
+      </div>
+    </div>
+    <!-- jQuery -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/sidebar_menu.js"></script>
+</body>
+</html>
