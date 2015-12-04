406786fbe8982e574987b4e41e00aa57da6844e0
flexus404 <flavien.trade18@gmail.com>
2015-12-04 05:44:29 +0100

    ajout de rechercher.php dans site

---
+++ index.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+    <title>Les Sockets Diurnes</title>
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Custom styles for this template -->
+    <link href="css/monStyle.css" rel="stylesheet">
+  </head>
+  <body>
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <div class="navbar-brand">La nuit de l'info - Equipe 6</div>
+        </div>
+        <form class="navbar-form navbar-right">
+          <a href="accueil.php" class="btn btn-success" role="button">Notre Projet</a>
+        </form>
+      </div> <!-- container -->
+    </nav> <!-- nav -->
+    <div class="jumbotron">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-3">
+            <img width="275" src="img/drawing.svg" ald="Generic placeholder image"></img>
+          </div>
+          <div class="col-lg-9">
+            <h1>Les Sockets Diurnes</h1>
+            <h4><i>LSD, parce qu'on en a besoin.</i></h4>
+            <br>
+            <h4><small>Vous êtes à la recherche de l'équipe de la bonne humeur et de l'efficacité contre-productive ? Vous êtes au bon endroit. Chez LSD on voit la vie en rose, en bleu, en violet ou même en vert ; n'importe quelle couleur du moment que vous l'aimez, et on vous aime aussi.
+				<br />Quoique vous puissiez aimer, LSD pourra vous montrer des choses que vous aimerez encore plus, alors laissez-vous tenter.</small></h4>
+			<h4><small>Les membres de l'équipe en font tous partie de leur plein gré et l'existence d'une technologie permettant de les enfermer dans de petites boules contre leur volonté n'a aucun rapport avec leur présence.</small></h4>
+          </div>
+        </div>
+      </div><!-- /.container -->
+    </div><!-- jumbotron -->
+    <div class="container marketing">
+      <div class="row">
+        <div class="col-lg-3">
+          <img class="img-circle" width="140" height="140" alt="Generic placeholder image"
+          src="img/capy.png"><img>
+          <h2>Marie Mion </h2>
+          <h4>5A STI</h4>
+          <h4>Chef d'équipe / Front</h4>
+          <em>"Il faut toujours se méfier d'un truc qui peut saigner quatre jours sans mourir"</em>
+        </div>
+        <div class="col-lg-3">
+          <img class="img-circle" width="140" height="140" alt="Generic placeholder image"
+          src="img/pika.jpg"></img>
+          <h2>Loic Daval </h2>
+          <h4>4A STI</h4>
+          <h4>Développeur PHP</h4>
+          <em>"Le plus important n'est pas ce que tu es, mais ce que tu as choisi d'être."</em>
+        </div>
+        <div class="col-lg-3">
+          <img class="img-circle" width="140" height="140" alt="Generic placeholder image"
+          src="img/sala.png"><img>
+          <h2>Souhail Zaimi </h2>
+          <h4>4A STI</h4>
+          <h4>Rôle dans l'équipe</h4>
+          <em>"Citation"</em>
+        </div>
+        <div class="col-lg-3">
+          <img class="img-circle" width="140" height="140" alt="Generic placeholder image"
+          src="img/cara.jpg"><img>
+          <h2>Issam Zriouil </h2>
+          <h4>4A STI</h4>
+          <h4>Rôle dans l'équipe</h4>
+          <em>"Citation"</em>
+        </div>
+	</div>
+	<div class="row">
+	<br><br>
+        <div class="col-lg-3">
+          <img class="img-circle" width="140" height="140" alt="Generic placeholder image"
+          src="img/fantominus.png"><img>
+          <h2>Justin Hocquel </h2>
+          <h4>3A STI</h4>
+          <h4>Esprit hanteur</h4> <!-- Lisez la citation avec et sans le rôle... -->
+          <em>"Les belles sont rarement libres et les libres rarement belles"</em>
+        </div>
+        <div class="col-lg-3">
+          <img class="img-circle" width="140" height="140" alt="Generic placeholder image"
+          src="img/Flagadoss.png"><img>
+          <h2>Guillaume Foreau </h2>
+          <h4>3A STI</h4>
+          <h4>Mascotte et nounours<br />Takes care of back ends</h4>
+          <em>"Free Hugs for all !"</em>
+        </div>
+        <div class="col-lg-3">
+          <img class="img-circle" width="140" height="140" alt="Generic placeholder image"
+          src="img/chat.jpg"><img>
+          <h2>Jordan Salaun </h2>
+          <h4>3A MRI</h4>
+          <h4>Back et Café</h4>
+          <em>"MRI et fier de l'être!"</em>
+        </div>
+        <div class="col-lg-3">
+          <img class="img-circle" width="140" height="140" alt="Generic placeholder image"
+          src="img/TheFlash.png"><img>
+          <h2>Yann Caumartin </h2>
+          <h4>2A STI</h4>
+          <h4>Optimist</h4> <!-- blague Optimist/Optimiste, drôle, toussa toussa,... -->
+          <em>"Le pessimiste se plaint du vent, l'optimiste espère qu'il va changer, le réaliste ajuste ses voiles."</em>
+        </div>
+	</div>
+	<div class="row">
+	<br><br>
+	<div class="col-lg-3"></div>
+        <div class="col-lg-3">
+          <img class="img-circle" width="140" height="140" alt="Generic placeholder image"
+          src="img/emeu.jpg"><img>
+          <h2>Camille Chastain </h2>
+          <h4>2A STI</h4>
+          <h4>Paraît que je suis en front-End. C'est quoi front-End? Je demanderai à FRJ</h4>
+          <em>"Pense à la solution, pas au problème. Si ton esprit est rempli de raisons d'échouer, impossible de réfléchir à une façon de réussir"</em>
+        </div>
+        <div class="col-lg-3">
+          <img class="img-circle" width="140" height="140" alt="Generic placeholder image"
+          src="img/frj.jpg"><img>
+          <h2>Flavien Ronteix--Jacquet </h2>
+          <h4>2A STI</h4>
+          <h4>Adorateur d'ornithorynque notoire et au front-end à mes heures perdues</h4>
+          <em>"Il n'est pas scientifiquement prouvé que l'ornithorynque développe une intelligence, mais il stimule incontestablement notre curiosité!"</em>
+        </div>
+	</div>
+      </div>
+    </div>
+      <!-- Bootstrap core JavaScript
+      ================================================== -->
+      <!-- Placed at the end of the document so the pages load faster -->
+  <script src="js/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
